c778e1d4ed17853b2995f724966fa91ab5703fea
Dumindu Karunathilaka <dumindukarunathilaka@gmail.com>
2018-02-18 20:23:00 +0530

    add structure to http monitor

--- monitors/http.monitor.js
+++ monitors/http.monitor.js
@@ -1,3 +1,35 @@
-/**
- * Created by duminduk on 2/15/18.
- */
+const request = require('request');
+const net = require('net');
+const logger = require('../utils/logger');
+
+function getStatusOfApplication(application) {
+    return new Promise((resolve) => {
+        const connection = net.connect({ host: 'localhost', port: application.port }, () => {
+            connection.destroy();
+            logger.info(`successfully connected to ${application.name}`);
+            resolve({
+                name: application.name,
+                status: 'online',
+                type: 'tcp'
+            });
+        });
+
+        connection.on('error', (err) => {
+            connection.destroy();
+            logger.error(`error while connecting to ${application.name}`);
+            logger.error(err);
+            resolve({
+                name: application.name,
+                status: 'offline',
+                type: 'tcp'
+            });
+        });
+    });
+}
+
+async function getStatus(applications) {
+    return Promise.all(applications.map(application =>
+        getStatusOfApplication(application)));
+}
+
+module.exports = { getStatus };
